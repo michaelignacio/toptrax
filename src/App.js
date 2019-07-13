215bec481738364939c38a9608bc933a3135bc21
Michael Ignacio <mikelignacio@gmail.com>
2019-07-14 02:44:13 +0800

    Add React components Update styles Add SASS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+function ChartItem() {
+  return (
+    <div className="chart-item">
+      <ChartPosition/>
+      <ArtistPhoto/>
+      <SongDetails/>
+    </div>
+  );
+}
+
+function SongDetails() {
+  let titleStyle = { fontWeight: "700", marginBottom: "5px", marginTop: "0" }
+  return (
+      <div style={{ paddingLeft: "20px", marginTop: "20px" }}>
+        <p style={titleStyle}>Old Town Road</p>
+        <p style={{ fontSize: ".9rem", marginTop: "5px" }}>Lil Nas X ft. Billy Ray Cyrus</p>
+      </div>
+  );
+}
+
+function ChartPosition() {
+  return (
+      <div className="position"> 1 </div>
+  );
+}
+
+function ArtistPhoto() {
+  return (
+      <div style={{ padding: "5px" }}>
+        <img src="https://charts-static.billboard.com/img/2019/03/lil-nas-x-8wa-87x87.jpg" alt=""/>
+      </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChartItem />
     </div>
   );
 }
