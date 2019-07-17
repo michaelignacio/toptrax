8592f35fd7f9f4af0f3ed2d1073fbc10700f8264
Michael Ignacio <mikelignacio@gmail.com>
2019-07-17 08:53:03 +0800

    Update index

--- src/App.js
+++ src/App.js
@@ -141,13 +141,8 @@ class App extends Component {
   render () {
     return (
       <div className={this.state.isFetched ? 'App logged-in' : 'App' }>
-        <MetaTags>
-          <title>TopTrax - Know Your Top Spotify Tracks</title>
-          <meta property="og:title" content="TopTrax" />
-          <meta property="og:url" content="http://toptrax.herokuapp.com/" />
-          <meta property="og:image" content={toptraxImage} />
-          <meta property="og:type" content="website" />
-        </MetaTags>
+        {/*<MetaTags>
+        </MetaTags>*/}
 
         {this.state.isFetched ?
           <Chart data={this.state}/>
