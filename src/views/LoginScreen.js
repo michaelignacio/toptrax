3c58c2348073c4be38c7455694fc6ddc634ff8e9
Michael Ignacio <mikelignacio@gmail.com>
2019-07-31 00:10:07 +0800

    Update button icons

--- src/views/LoginScreen.js
+++ src/views/LoginScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
-import githubMark from '../images/github.png';
-import spotifyIcon from '../images/spotify.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpotify, faGithub } from '@fortawesome/free-brands-svg-icons';
 import styles from '../styles/Type.module.css';
 import btnStyles from '../styles/Button.module.css';
 import loginStyles from '../styles/LoginScreen.module.css';
@@ -19,9 +19,11 @@ const LoginScreen = (props) => {
         <h1 className={styles.h1Style}>TopTrax</h1>
         <h2 className={styles.h2Style}>Discover your most played tracks on Spotify</h2>
 
-        <a className={btnStyles.btn} href={process.env.NODE_ENV === 'production' ? process.env.REACT_APP_HEROKU_LOGIN : process.env.REACT_APP_LOCAL_LOGIN}>Log in with Spotify <img src={spotifyIcon} alt="Spotify Icon" width="28" height="28" /></a>
+        <a className={btnStyles.btn} href={process.env.NODE_ENV === 'production' ? process.env.REACT_APP_HEROKU_LOGIN : process.env.REACT_APP_LOCAL_LOGIN}>
+          <span>Log in with Spotify</span> <FontAwesomeIcon icon={faSpotify} className={btnStyles.fa} />
+        </a>
 
-        <a className={`${btnStyles.btn} ${btnStyles.github}`} target="_blank" rel="noopener noreferrer" href="https://github.com/michaelignacio/toptrax">View Source Code <img src={githubMark} alt="Github Mark" width="28" height="28" /></a>
+        <a className={`${btnStyles.btn} ${btnStyles.github}`} target="_blank" rel="noopener noreferrer" href="https://github.com/michaelignacio/toptrax">View Source Code <FontAwesomeIcon icon={faGithub} className={btnStyles.fa} /></a>
       </div>
     </CSSTransitionGroup>
   );
