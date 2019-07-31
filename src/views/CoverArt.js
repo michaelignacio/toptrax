017974f55cb8b7db8657c36a03dacabfb94025a6
Michael Ignacio <mikelignacio@gmail.com>
2019-07-31 11:04:02 +0800

    Add play icon

--- src/views/CoverArt.js
+++ src/views/CoverArt.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/ChartItem.module.css';
 
 const CoverArt = (props) => {
   return (
-      <div className={styles.CoverArt}>
+      <div className={styles.coverArt}>
         <img src={props.photo.images[1].url} width="128" height="128" alt={props.photo.name} />
+        <FontAwesomeIcon icon={ faPlay } className={styles.icon} />
       </div>
   );
 }
